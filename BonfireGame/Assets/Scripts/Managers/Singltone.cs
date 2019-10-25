be32d1dda4f1c956369b95ac3092d9c53aed6103
Andrey Denisenko <francescogacczi@gmail.com>
2019-10-25 22:42:25 +0300

    day_two

---
+++ BonfireGame/Assets/Scripts/Managers/Singltone.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Singltone<T> : MonoBehaviour where T : MonoBehaviour
+{
+    private static T _Instance;
+
+    public static T Instance
+    {
+        get
+        {
+            if (!_Instance)
+            {
+                _Instance = FindObjectOfType<T>();
+            }
+
+            if (_Instance)
+            {
+                Debug.Log("Cant find " + typeof(T));
+            }
+            return _Instance;
+        }
+    }
+}
---
+++ BonfireGame/Assets/Scripts/Managers/UIManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIManager : Singltone<UIManager>
+{
+    public RectTransform inventoryPanel;
+    public RectTransform mainMenuPanel;
+    public RectTransform gamePlayPanel;
+    public GameObject gameplayElements;
+
+    public void openInventory()
+    {
+        Animator animator = inventoryPanel.GetComponent<Animator>();
+        if (animator)
+        {
+            bool openInventoryUI = animator.GetBool("OpenInventory");
+            animator.SetBool("OpenInventory", !openInventoryUI);
+        }
+    }
+
+    public void openMainMenu()
+    {
+        Animator animator = mainMenuPanel.GetComponent<Animator>();
+        if (animator)
+        {
+            bool openMainMenuUI = animator.GetBool("OpenMainMenu");
+            gamePlayPanel.gameObject.SetActive(openMainMenuUI);
+            gameplayElements.gameObject.SetActive(openMainMenuUI);
+            animator.SetBool("OpenMainMenu", !openMainMenuUI);
+        }
+    }
+}
